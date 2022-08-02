38ac38176d679ac7ac79a6e76e33581c152bdb81
Roshanhelal <98272607+Roshanhelal@users.noreply.github.com>
2022-08-02 21:37:25 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,13 +5,30 @@ function preload(){
 function setup(){
     canvas=createCanvas(300,300);
     canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet=ml5.poseNet(video,modelloaded);
+    poseNet.on('pose',gotposes);
 }
 
 function draw(){
-    
+    image(video,0,0,300,300);
 }
 
 function take_snapshot(){
     save('selfie.png')
 }
 
+function modelloaded(){
+    console.log("poseNet is initialized");
+}
+
+function gotposes(results){
+    if (results.length>0) {
+        console.log(results);
+        console.log("x position of the nose is "+results[0].pose.nose.x);
+        console.log("y position of the nose is "+results[0].pose.nose.y);
+    }
+}
